6c98a6f4186e137463615d4eb40123d32c81056f
Ubeyd-22 <uabuka20@student.aau.dk>
2022-12-15 13:23:00 +0100

    changes to con.cc, added tracing of congestion window and Rtt

--- con.cc
+++ con.cc
@@ -1,18 +1,97 @@
+static char const *scenario = "Congestion";
 #include "includes/general_setup.cc"
 // #include "includes/tracing.cc"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+/*
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/flow-monitor-module.h"
 
-NS_LOG_COMPONENT_DEFINE ("TcpBulkSendExample");
+using namespace ns3;
+*/
 
 
+//NS_LOG_COMPONENT_DEFINE ("TcpBulkSendExample");
+
+static std::map<uint32_t, bool> firstCwnd;
+static std::map<uint32_t, bool> firstRtt;
+static std::map<uint32_t, Ptr<OutputStreamWrapper>> cWndStream;
+static std::map<uint32_t, Ptr<OutputStreamWrapper>> rttStream;
+static std::map<uint32_t, uint32_t> cWndValue;
+
+static uint32_t
+GetNodeIdFromContext (std::string context)
+{
+  std::size_t const n1 = context.find_first_of ("/", 1);
+  std::size_t const n2 = context.find_first_of ("/", n1 + 1);
+  return std::stoul (context.substr (n1 + 1, n2 - n1 - 1));
+}
+
+static void
+CwndTracer (std::string context,  uint32_t oldval, uint32_t newval)
+{
+  //NS_LOG_INFO("Cwnd Started");
+  uint32_t nodeId = GetNodeIdFromContext (context);
+
+  if (firstCwnd[nodeId])
+    {
+      *cWndStream[nodeId]->GetStream () << "0.0 " << oldval << std::endl;
+      firstCwnd[nodeId] = false;
+    }
+  *cWndStream[nodeId]->GetStream () << Simulator::Now ().GetSeconds () << " " << newval << std::endl;
+  cWndValue[nodeId] = newval;
+}
+
+static void
+TraceCwnd (std::string cwnd_tr_file_name, uint32_t nodeId)
+{
+  //NS_LOG_INFO("TraceCwnd Start");
+  AsciiTraceHelper ascii;
+  cWndStream[nodeId] = ascii.CreateFileStream (cwnd_tr_file_name.c_str ());
+  Config::Connect ("/NodeList/" + std::to_string (nodeId) + "/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow",
+                   MakeCallback (&CwndTracer));
+}
+
+static void
+RttTracer (std::string context, Time oldval, Time newval)
+{
+  uint32_t nodeId = GetNodeIdFromContext (context);
+
+  if (firstRtt[nodeId])
+    {
+      *rttStream[nodeId]->GetStream () << "0.0 " << oldval.GetSeconds () << std::endl;
+      firstRtt[nodeId] = false;
+    }
+  *rttStream[nodeId]->GetStream () << Simulator::Now ().GetSeconds () << " " << newval.GetSeconds () << std::endl;
+}
+
+static void
+TraceRtt (std::string rtt_tr_file_name, uint32_t nodeId)
+{
+  AsciiTraceHelper ascii;
+  rttStream[nodeId] = ascii.CreateFileStream (rtt_tr_file_name.c_str ());
+  Config::Connect ("/NodeList/" + std::to_string (nodeId) + "/$ns3::TcpL4Protocol/SocketList/0/RTT",
+                   MakeCallback (&RttTracer));
+}
 
 
 int
 run (string tcp_version)
 {
 
-  bool tracing = false;
+  //bool tracing = false;
   uint32_t maxBytes = 0;
+  std::string prefix_file_name = tcp_version;
 
 
 //
@@ -60,7 +139,9 @@ run (string tcp_version)
 
   NetDeviceContainer d0d2 = pointToPoint.Install (n0n2);
   NetDeviceContainer d1d2 = pointToPoint.Install (n1n2);
+  //pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Kbps"));
   NetDeviceContainer d2d3 = pointToPoint.Install (n2n3);
+  //pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("500Kbps"));
   NetDeviceContainer d3d4 = pointToPoint.Install (n3n4);
   NetDeviceContainer d3d5 = pointToPoint.Install (n3n5);
 //
@@ -125,7 +206,7 @@ Ipv4GlobalRoutingHelper::PopulateRoutingTables();
   ApplicationContainer sourceApps = source.Install (nodes.Get (0));
   sourceApps.Add(source2.Install(nodes.Get(1)));
   sourceApps.Start (Seconds (0.0));
-  sourceApps.Stop (Seconds (10.0));
+  sourceApps.Stop (Seconds (190.0));
 
 //
 // Create a PacketSinkApplication and install it on node 1
@@ -137,31 +218,49 @@ Ipv4GlobalRoutingHelper::PopulateRoutingTables();
   PacketSinkHelper sink2 ("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
   sinkApps.Add(sink2.Install(nodes.Get(5)));
   sinkApps.Start (Seconds (0.0));
-  sinkApps.Stop (Seconds (10.0));
+  sinkApps.Stop (Seconds (190.0));
 
 //
 // Set up tracing if enabled
 //
+  /*
   if (tracing)
     {
       AsciiTraceHelper ascii;
-      pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("tcp-bulk-send.tr"));
-      pointToPoint.EnablePcapAll ("tcp-bulk-send", false);
+      pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("Congestion.tr"));
+      pointToPoint.EnablePcapAll ("Congestion", false);
     }
-
+*/
 //
 // Capture packets: create pcap files. 
-
+  /*
   AsciiTraceHelper ascii;
-  Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream("tcp-bulk-send.tr");
+  Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream("Congestion.tr");
   pointToPoint.EnableAsciiAll(stream);
-  pointToPoint.EnablePcapAll("tcp-bulk-send");
-
+  pointToPoint.EnablePcapAll("Congestion");
+*/
 //
 // Now, do the actual simulation.
 //
+ // Setup tracing for congestion window on node 0 and write results to file
+ if (tcp_version == "Bbr") {
+
+ }
+  Simulator::Schedule (Seconds (start_time + 0.00001), &TraceCwnd,
+                       prefix_file_name + "-cwnd.data", 0);
+  // Setup tracing for RTT
+  Simulator::Schedule (Seconds (start_time + 0.00001), &TraceRtt,
+                       prefix_file_name + "-rtt.data", 0);
+
+  Simulator::Schedule (Seconds (start_time + 0.00001), &TraceCwnd,
+                       prefix_file_name + "-cwnd2.data", 1);
+  // Setup tracing for RTT
+  Simulator::Schedule (Seconds (start_time + 0.00001), &TraceRtt,
+                       prefix_file_name + "-rtt2.data", 1);
+
+
   NS_LOG_INFO ("Run Simulation.");
-  Simulator::Stop (Seconds (10.0));
+  Simulator::Stop (Seconds (200.0));
   Simulator::Run ();
   Simulator::Destroy ();
   NS_LOG_INFO ("Done.");
